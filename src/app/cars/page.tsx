3f59fd7ef0dce80d550c82cda0ce9b832c475105
pavlosidukov <NoooB199505@gmail.com>
2025-02-10 10:46:58 +0200

    homework

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -2,15 +2,15 @@ import {FC} from "react";
 import CarForm from "@/components/carForm/CarForm";
 import Cars from "@/components/cars/Cars";
 import { addCar } from "@/server-actions/serverActions";
+import {ICar} from "@/interfaces/carInterface";
 
 type Props = {
-    searchParams: Promise<{
-        [key: string]: string | string[] | undefined;
-    }>
+    searchParams: Promise<ICar>
 }
 
 const CarsPage:FC<Props> = async ({searchParams}) => {
     const awaited = await searchParams
+    console.log(awaited)
     await addCar(awaited)
 
     return (
